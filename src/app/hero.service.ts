fc012e5c62c72fcea848cd2ee4f12c6fd995bf05
Pascal Pommier <ppommier@akare.eu>
2019-01-19 09:43:22 +0100

    fin traduction module service/provider

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,7 +13,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add('HeroService: fetched '+HEROES.length+' Heroes');
     return of(HEROES);
   }
   
